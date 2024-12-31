603b204111fb9499b24b48294c75323288539ad4
Cruz Núñez <cruz@Cruzs-15-MacBook-Pro.local>
2024-12-31 16:37:44 -0500

    add Los Angles Homeless Services Authority scraper

---
+++ city_scrapers/spiders/losca_Homeless_Services.py
@@ -0,0 +1,106 @@
+from city_scrapers_core.constants import BOARD, CITY_COUNCIL, COMMISSION, COMMITTEE, NOT_CLASSIFIED
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse
+from datetime import datetime
+from urllib.parse import urljoin
+
+
+class LoscaHomelessServicesSpider(CityScrapersSpider):
+    name = "losca_Homeless_Services"
+    agency = "Los Angeles Homeless Services Authority"
+    timezone = "America/Los_Angeles"
+    start_urls = ["https://www.lahsa.org/events"]
+
+    def parse(self, response):
+        """
+        Parse upcoming meetings for LAHSA.
+        """
+        for item in response.css(".col-lg-8 .list-group a"):
+            # parse classification first
+            # we need it for location
+            classification = self._parse_classification(item)
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description="",
+                classification=classification,
+                start=self._parse_start(item),
+                end=self._parse_end(item),
+                all_day=False,
+                time_notes="Start time in Event Link",
+                location=self._parse_location(classification),
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse meeting title."""
+        return item.css('.h6::text').get()
+
+    def _parse_classification(self, item):
+        """Parse classification from allowed options."""
+        title = item.css('.h6::text').get().lower()
+        if "board" in title:
+            return BOARD
+        elif "council" in title:
+            return CITY_COUNCIL
+        elif "commission" in title:
+            return COMMISSION
+        elif "committee" in title:
+            return COMMITTEE
+        else: return NOT_CLASSIFIED
+
+        return NOT_CLASSIFIED
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        # import pdb; pdb.set_trace()
+        datestring = item.css('.text-secondary::text').get()
+        # sometimes datestring is "NEXT WEDNESDAY"
+        # use fuzzy parser to handle this kind of input
+        # example: if today is 12/20/2024, "NEXT WEDNESDAY" returns "12/25/2024"
+        # sometimes datestring is "January 08" which returns "1/8/2024"
+        # but we need it to return "1/8/2025"
+        date = parse(datestring, fuzzy=True)
+        if date < datetime.today():
+            date = date.replace(year=date.year + 1)
+        return date
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        return None
+
+    def _parse_location(self, classification):
+        """
+        Generate location from classification.
+        After inspecting the locations from different agency meetings,
+        it appears only city council meetings occur at a different address.
+        """
+        # default location
+        location = {
+            "name": "LAHSA (check Event Link)",
+            "address": "707 Wilshire Blvd, 10th Floor, Los Angeles, CA 90017"
+        }
+
+        # override if classification is city council
+        if classification == CITY_COUNCIL:
+            location["name"] = "City Council (check Event Link)"
+            location["address"] = "637 Wilshire Blvd, 1st Floor Commission Room, Los Angeles, CA 90017"
+
+        return location
+
+    def _parse_links(self, item):
+        """Parse links."""
+        base_url = "https://www.lahsa.org/"
+        href = item.css('::attr(href)').get()
+        url = urljoin(base_url, href)
+        return [{"title": "Event Link", "href": url}]
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
---
+++ tests/test_losca_Homeless_Services.py
@@ -0,0 +1,120 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD, CITY_COUNCIL, COMMISSION, COMMITTEE, NOT_CLASSIFIED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.losca_Homeless_Services import LoscaHomelessServicesSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "losca_Homeless_Services.html"),
+    url="https://www.lahsa.org/events",
+)
+spider = LoscaHomelessServicesSpider()
+
+freezer = freeze_time("2024-12-20")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+def test_count():
+    assert len(parsed_items) == 15
+
+def test_title():
+    assert parsed_items[0]["title"] == "CES Policy Council Meeting - Canceled"
+    assert parsed_items[1]["title"] == "LA COC BOARD HMIS COMMITTEE MEETING"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2024, 12, 25, 0, 0)
+    assert parsed_items[1]["start"] == datetime(2025, 1, 8, 0, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] == None
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == "Start time in Event Link"
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "losca_Homeless_Services/202412250000/x/ces_policy_council_meeting"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "cancelled"
+    assert parsed_items[1]["status"] == "tentative"
+
+
+def test_location():
+    # if committee or commission or board
+        # LAHSA, 707 Wilshire Blvd, 10th Floor, Los Angeles, CA 90017
+    # if council
+        # 637 Wilshire Blvd, 1st Floor Commission Room, Los Angeles, CA 90017
+    # city council is the only agency that doesn't meet at usual LAHSA location
+    assert parsed_items[0]["classification"] == CITY_COUNCIL
+    assert parsed_items[0]["location"] == {
+        "name": "City Council (check Event Link)",
+        "address": "637 Wilshire Blvd, 1st Floor Commission Room, Los Angeles, CA 90017"
+    }
+    # board uses default location
+    assert parsed_items[1]["classification"] == BOARD
+    assert parsed_items[1]["location"] == {
+        "name": "LAHSA (check Event Link)",
+        "address": "707 Wilshire Blvd, 10th Floor, Los Angeles, CA 90017"
+    }
+    # committee uses default location
+    assert parsed_items[3]["classification"] == COMMITTEE
+    assert parsed_items[3]["location"] == {
+        "name": "LAHSA (check Event Link)",
+        "address": "707 Wilshire Blvd, 10th Floor, Los Angeles, CA 90017"
+    }
+    # commission uses default location
+    assert parsed_items[5]["classification"] == COMMISSION
+    assert parsed_items[5]["location"] == {
+        "name": "LAHSA (check Event Link)",
+        "address": "707 Wilshire Blvd, 10th Floor, Los Angeles, CA 90017"
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://www.lahsa.org/events"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        "title": "Event Link",
+        "href": "https://www.lahsa.org/events?e=1412-ces-policy-council-meeting-canceled"
+    }]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == CITY_COUNCIL
+    assert parsed_items[1]["classification"] == BOARD
+    assert parsed_items[2]["classification"] == BOARD
+    assert parsed_items[3]["classification"] == COMMITTEE
+    assert parsed_items[4]["classification"] == COMMITTEE
+    assert parsed_items[5]["classification"] == COMMISSION
+    assert parsed_items[6]["classification"] == CITY_COUNCIL
+    assert parsed_items[7]["classification"] == COMMITTEE
+    assert parsed_items[8]["classification"] == COMMITTEE
+    assert parsed_items[9]["classification"] == CITY_COUNCIL
+    assert parsed_items[10]["classification"] == COMMISSION
+    assert parsed_items[11]["classification"] == COMMITTEE
+    assert parsed_items[12]["classification"] == COMMITTEE
+    assert parsed_items[13]["classification"] == CITY_COUNCIL
+    assert parsed_items[14]["classification"] == COMMISSION
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
